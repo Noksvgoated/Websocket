5d0d519e0704425b7ee7aa1222b557cddfc52881
Noksvgoated <finderbase67@gmail.com>
2026-09-09 13:25:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ export default {
         const url = new URL(request.url);
         
         if (url.pathname === '/api/online') {
-            const key = url.searchParams.get('key');
-            if (key !== env.SHOPIFY_SECRET_KEY) {
-                return new Response(JSON.stringify({
-                    success: false,
-                    error: 'Invalid password'
-                }), { status: 401 });
-            }
-            
             return new Response(JSON.stringify({
                 success: true,
                 count: 0,
@@ -26,14 +18,6 @@ export default {
             const upgradeHeader = request.headers.get('Upgrade');
             if (upgradeHeader !== 'websocket') {
                 return new Response('Expected WebSocket', { status: 426 });
-            }
-            
-            const key = url.searchParams.get('key');
-            if (key !== env.SHOPIFY_SECRET_KEY) {
-                return new Response(JSON.stringify({
-                    success: false,
-                    error: 'Invalid password'
-                }), { status: 401 });
             }
             
             const id = env.LISTING_ROOM.idFromName('global-room');
